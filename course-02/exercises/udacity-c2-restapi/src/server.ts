33f2870c0c5f4b76ef6626d8d1ca84402207982c
Cinthia Valero <cvalero@anditi.com>
2021-07-20 18:14:05 +1000

    fixed exercise

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -16,16 +17,17 @@ import { V0MODELS } from "./controllers/v0/model.index";
   const port = process.env.PORT || 8080; // default port to listen
 
   app.use(bodyParser.json());
+  app.use(cors());
 
   //CORS Should be restricted
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    next();
-  });
+  // app.use(function (req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  //   res.header(
+  //     "Access-Control-Allow-Headers",
+  //     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  //   );
+  //   next();
+  // });
 
   app.use("/api/v0/", IndexRouter);
 
